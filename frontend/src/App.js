fd79c63c85a5261274c81b0f9454381b7a58be8d
guru2711 <saiguruprakash@gmail.com>
2024-04-06 21:04:01 +0530

    front End flow setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import { Container } from "react-bootstrap"
+import { Outlet } from "react-router-dom"
 import  Header  from "./components/Header"
 import Footer from "./components/Footer"
-import HomeScreen from "./screens/HomeScreen"
+
 
 export const App = () => {
   return (
@@ -10,7 +11,8 @@ export const App = () => {
       <main className="py-3">
         <Container>
 
-     <HomeScreen />
+    
+     <Outlet />
         </Container>
       </main>
       <Footer />
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,32 @@
 import {Navbar, Nav, Container } from 'react-bootstrap'
 import {FaShoppingCart, FaUser } from 'react-icons/fa'
 import logo from "../assets/logo.png"
+import {LinkContainer} from "react-router-bootstrap"
 
  const Header = () => {
   return (
     <header>
         <Navbar bg='dark' variant='dark' expand="md" collapseOnSelect>
 <Container>
-    <Navbar.Brand href="/">
+    <LinkContainer to="/">
+    <Navbar.Brand >
         <img src={logo} alt='Guru'/>
         Guru
         </Navbar.Brand>
+    </LinkContainer>
     <Navbar.Toggle aria-controls='basic-navbar-nav' />
     <Navbar.Collapse id="basic-navbar-nav">
     <Nav className = 'ms-auto'>
-        <Nav.Link href="/cart">
+        <LinkContainer to="/cart">
+        <Nav.Link >
             <FaShoppingCart/> Cart 
             </Nav.Link>
-        <Nav.Link href="/login">
+        </LinkContainer>
+        <LinkContainer to="/login">
+        <Nav.Link >
             <FaUser/> Sign in 
             </Nav.Link>
+        </LinkContainer>
     </Nav>
     </Navbar.Collapse>
 </Container>
